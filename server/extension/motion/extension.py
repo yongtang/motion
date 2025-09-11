f86a7a8cf9535da033eb4d875e1bfbc1b5400253
Yong Tang <yong.tang.github@outlook.com>
2025-09-11 04:01:06 +0000

    Fix

--- server/extension/motion/extension.py
+++ server/extension/motion/extension.py
@@ -164,7 +164,7 @@ class MotionExtension(omni.ext.IExt):
     def on_startup(self, ext_id):
         print(f"[motion.extension] Startup [{ext_id}]")
 
-        self.task = omni.kit.async_engine.run_coroutine(main())
+        self.task = asyncio.create_task(main()) #omni.kit.async_engine.run_coroutine(main())
 
         def f_done(e: asyncio.Task):
             if e.exception() is not None:
