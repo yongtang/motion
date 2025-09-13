b967283efb705ab7fe54a0ec0418c7c65ca37cdb
Yong Tang <yong.tang.github@outlook.com>
2025-09-13 04:05:52 +0000

    Work enhancement

--- server/work.py
+++ server/work.py
@@ -14,51 +14,37 @@ from .channel import Channel
 from .storage import storage_kv_get, storage_kv_set
 
 logging.basicConfig(level=logging.INFO)
-log = logging.getLogger("worker")
+log = logging.getLogger(__name__)
 
 
-@contextlib.asynccontextmanager
-async def run_done(session: str):
+async def node_play(channel: Channel, session: str):
     shutil.rmtree("/storage/node", ignore_errors=True)
     os.makedirs("/storage/node/scene", exist_ok=True)
 
-    session = json.loads(b"".join(storage_kv_get("session", f"{session}.json")))
+    data = json.loads(b"".join(storage_kv_get("session", f"{session}.json")))
     with open("/storage/node/session.json", "w") as f:
-        f.write(json.dumps(session))
-    log.info(f"[run_done]: session storage: {session}")
+        f.write(json.dumps(data))
+    log.info(f"[node_play]: session storage: {data}")
+
+    scene = data["scene"]
+    log.info(f"[node_play]: scene={scene}")
 
     with tempfile.TemporaryFile() as f:
-        for chunk in storage_kv_get("scene", f"{session['scene']}.zip"):
+        for chunk in storage_kv_get("scene", f"{scene}.zip"):
             f.write(chunk)
         f.seek(0)
         with zipfile.ZipFile(f) as z:
             z.extractall("/storage/node/scene")
-    log.info(f"[run_done]: uncompress scene")
+    log.info(f"[node_play]: uncompress scene")
 
     with open("/storage/node/scene/meta.json", "r") as f:
         meta = json.loads(f.read())
     runtime = meta["runtime"]
-    log.info(f"[run_done]: runtime={runtime}")
+    log.info(f"[node_play]: runtime={runtime}")
 
     scope = os.environ.get("SCOPE")
     project = f"{scope}-motion" if scope else "motion"
 
-    try:
-        yield runtime
-    finally:
-        done = ["docker", "compose", "-p", project, "rm", "-f", "-s", "node"]
-        log.info(f"[run_done]: done={done}")
-        proc = await asyncio.create_subprocess_exec(*done, env={**os.environ})
-
-        await proc.wait()
-        shutil.rmtree("/storage/node", ignore_errors=True)
-        log.info(f"[run_done]: done")
-
-
-@contextlib.asynccontextmanager
-async def run_node(runtime: str):
-    scope = os.environ.get("SCOPE")
-    project = f"{scope}-motion" if scope else "motion"
     node = [
         "docker",
         "compose",
@@ -74,49 +60,11 @@ async def run_node(runtime: str):
         "node",
     ]
     log.info(f"[run_node]: node={node}")
-    proc = await asyncio.create_subprocess_exec(*node, env={**os.environ})
-
-    try:
-        yield proc
-    finally:
-        log.info(f"[run_node]: node")
+    return await asyncio.create_subprocess_exec(*node, env={**os.environ})
 
 
-@contextlib.asynccontextmanager
-async def run_data(session: str):
-    with tempfile.NamedTemporaryFile(prefix=f"{session}-", suffix=".json") as f:
-        log.info(f"[run_data] temp={f.name}")
-        channel = Channel()
-        await channel.start()
-        sub = await channel.subscribe_data(session, start=1)
-        log.info(f"[run_data] session={session} channel start")
-
-        try:
-            yield
-        finally:
-            while True:
-                try:
-                    msg = await sub.next_msg(timeout=30.0)
-                except asyncio.TimeoutError:
-                    break
-                f.write(msg.data + b"\n")
-
-            await sub.unsubscribe()
-
-            f.seek(0)
-            storage_kv_set("data", f"{session}.json", f.read())
-            log.info(f"[run_data] uploaded s3://data/{session}.json")
-
-            await channel.close()
-            log.info(f"[run_data] session={session} channel close")
-
-
-async def run_stop(session: str):
-    log.info(f"[run_stop] session={session} stop")
-
-    with open("/storage/node/scene/meta.json", "r") as f:
-        meta = json.loads(f.read())
-    runtime = meta["runtime"]
+async def node_stop(channel: Channel, session: str):
+    log.info(f"[node_stop] session={session} stop")
 
     scope = os.environ.get("SCOPE")
     project = f"{scope}-motion" if scope else "motion"
@@ -129,13 +77,32 @@ async def run_stop(session: str):
         "stop",
         "node",
     ]
-    log.info(f"[run_stop] stop={stop}")
+    log.info(f"[node_stop] stop={stop}")
     proc = await asyncio.create_subprocess_exec(*stop, env={**os.environ})
 
     await proc.wait()
 
-    log.info(f"[run_stop] stop={stop}")
-    log.info(f"[run_stop] session={session} done")
+    with tempfile.NamedTemporaryFile(prefix=f"{session}-", suffix=".json") as f:
+        log.info(f"[node_stop] temp={f.name}")
+        sub = await channel.subscribe_data(session, start=1)
+        log.info(f"[node_stop] session={session} channel start")
+
+        try:
+            while True:
+                msg = await sub.next_msg(timeout=30.0)
+                f.write(msg.data + b"\n")
+        except asyncio.TimeoutError:
+            pass
+        finally:
+            await sub.unsubscribe()
+
+        f.seek(0)
+        storage_kv_set("data", f"{session}.json", f.read())
+        log.info(f"[node_stop] uploaded s3://data/{session}.json")
+
+    shutil.rmtree("/storage/node", ignore_errors=True)
+
+    log.info(f"[node_stop] session={session} done")
 
 
 @contextlib.asynccontextmanager
@@ -171,37 +138,38 @@ async def run_work():
         while True:
             log.info(f"[run_work]: play fetch")
             try:
-                play_msg = (await sub_play.fetch(batch=1, timeout=60))[0]
+                msg_play = (await sub_play.fetch(batch=1, timeout=60))[0]
             except (nats.errors.TimeoutError, IndexError):
                 continue
-            log.info(f"[run_work]: play msg={play_msg}")
+            log.info(f"[run_work]: play msg={msg_play}")
 
-            assert play_msg.subject.startswith(
-                "motion.node."
-            ) and play_msg.subject.endswith(".play")
-            session = play_msg.subject.removeprefix("motion.node.").removesuffix(
-                ".play"
+            assert msg_play.subject.startswith(
+                "motion.node.",
+            ) and msg_play.subject.endswith(
+                ".play",
+            )
+            session = msg_play.subject.removeprefix(
+                "motion.node.",
+            ).removesuffix(
+                ".play",
             )
             log.info(f"[run_work]: play session={session}")
-
-            sub_stop = await channel.subscribe_stop(session)
-
             try:
+                sub_stop = await channel.subscribe_stop(session)
+
                 log.info(f"[run_work]: play session={session}")
-                async with run_done(session) as runtime:
-                    async with run_data(session):
-                        async with run_node(runtime) as proc:
-                            try:
-                                stop_msg = await sub_stop.next_msg(timeout=3600)
-                            except nats.errors.TimeoutError:
-                                pass
-                            await run_stop(session)
-                            await asyncio.wait_for(proc.wait(), timeout=300)
+                proc = await node_play(channel, session)
+
+                try:
+                    msg_stop = await sub_stop.next_msg(timeout=3600)
+                except nats.errors.TimeoutError:
+                    pass
+                await node_stop(channel, session)
+
+                await asyncio.wait_for(proc.wait(), timeout=300)
+
                 log.info(f"[run_work]: play session={session} ack")
-                await play_msg.ack()
-            except Exception as e:
-                log.info(f"[run_work]: play session={session} e={e}")
-                raise
+                await msg_play.ack()
             finally:
                 await sub_stop.unsubscribe()
             log.info(f"[run_work]: play session={session} done")
