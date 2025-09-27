4c2416491e172a90dd7a1add416f7d7e27a62376
Yong Tang <yong.tang.github@outlook.com>
2025-09-27 21:23:54 +0000

    Count

--- server/count.py
+++ server/count.py
@@ -1,5 +1,6 @@
 import asyncio
 import contextlib
+import itertools
 import json
 import logging
 
@@ -15,19 +16,17 @@ async def run_tick(session: str, interface: Interface, channel: Channel):
     Strict 1->1: on each Channel step, send to ZMQ and wait for one reply, then publish back.
     """
 
-    async def f(msg):
-        data = msg.data
-        log.info(f"[run_node] zmq send ({data})")
-        step = await interface.tick(data)
-        log.info(f"[run_node] zmq recv ({step})")
-        await channel.publish_data(session, step)
-        log.info(f"[run_node] zmq loop done")
+    for i in itertools.count():
+        data = json.dumps({"joint": {"count": i}}, sort_keys=True).encode()
 
-    sub = await channel.subscribe_step(session, f)
-    try:
-        await asyncio.Future()  # run forever
-    finally:
-        await sub.unsubscribe()
+        log.info(f"[run_tick] channel send ({data})")
+        await channel.publish_data(session, data)
+
+        log.info(f"[run_tick] zmq send ({data})")
+        step = await interface.tick(data)
+        log.info(f"[run_tick] zmq recv ({step})")
+
+        await asyncio.sleep(1)
 
 
 async def run_norm(session: str, interface: Interface, channel: Channel):
@@ -38,14 +37,6 @@ async def run_norm(session: str, interface: Interface, channel: Channel):
     In norm mode, drop on pressure: non-blocking send, ignore if ZMQ pipe is full.
     """
 
-    async def f(msg):
-        data = msg.data
-        log.info(f"[run_node] zmq send ({data})")
-        # Non-blocking send: if ROUTER/pipe is full (HWM), drop this update
-        await interface.send(data)
-
-    sub = await channel.subscribe_step(session, f)
-
     async def g():
         while True:
             step = await interface.recv()
@@ -54,12 +45,22 @@ async def run_norm(session: str, interface: Interface, channel: Channel):
 
     task = asyncio.create_task(g())
     try:
-        await asyncio.Future()
+
+        for i in itertools.count():
+            data = json.dumps({"joint": {"count": i}}, sort_keys=True).encode()
+
+            log.info(f"[run_tick] channel send ({data})")
+            await channel.publish_data(session, data)
+
+            log.info(f"[run_tick] zmq send ({data})")
+            await interface.send(data)
+
+            await asyncio.sleep(1)
+
     finally:
         task.cancel()
         with contextlib.suppress(asyncio.CancelledError):
             await task
-        await sub.unsubscribe()
 
 
 async def run_node(session: str, tick: bool):
