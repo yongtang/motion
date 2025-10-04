69abc5cd6dc2d8a25109aaad2fca2e760f6c7e12
Yong Tang <yong.tang.github@outlook.com>
2025-10-04 06:50:58 +0000

    Publisher

--- server/ros/src/motion/motion/motion.py
+++ server/ros/src/motion/motion/motion.py
@@ -1,5 +1,37 @@
-def main():
-    print("Hi from motion.")
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class Motion(Node):
+
+    def __init__(self):
+        super().__init__("motion")
+        self.publisher_ = self.create_publisher(String, "topic", 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Hello World: %d" % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = Motion()
+
+    rclpy.spin(node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
