ea4bb843043798b601ce5eace2b6e4da32671dcd
Yong Tang <yong.tang.github@outlook.com>
2025-10-04 06:56:16 +0000

    Subscriber

--- server/ros/src/motion/motion/motion.py
+++ server/ros/src/motion/motion/motion.py
@@ -7,6 +7,10 @@ class Motion(Node):
 
     def __init__(self):
         super().__init__("motion")
+        self.subscription = self.create_subscription(
+            String, "topic", self.listener_callback, 10
+        )
+
         self.publisher_ = self.create_publisher(String, "topic", 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -18,6 +22,9 @@ class Motion(Node):
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
 
 
 def main(args=None):
