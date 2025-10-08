f0992f806ac992b0eb798bdd020452dc5b7496b9
Yong Tang <yong.tang.github@outlook.com>
2025-10-08 01:11:58 +0000

    Fix

--- server/live.py
+++ server/live.py
@@ -15,12 +15,56 @@ async def main():
     with open(file, "r") as f:
         camera = json.loads(f.read())
 
-    live = ["parallel --halt now,done=1 --halt now,fail=1 -j 0 :::"]
+    argv = [
+        "parallel",
+        "--line-buffer",
+        "--tag",
+        "--halt",
+        "now,done=1",
+        "--halt",
+        "now,fail=1",
+        "-j",
+        "0",
+        ":::",
+    ]
+    vv = ["gst-launch-1.0"]
     for name, entry in camera.items():
         width, height = entry["width"], entry["height"]
-        live.append(f"echo {name} {width} {height}")
+        location = name.removeprefix("/").replace("/", "_")
+        argv.append(
+            f"gst-launch-1.0 -e rtspsrc location=rtsp://127.0.0.1:8554/RTSPWriter_{location}_rgb protocols=tcp latency=200 tcp-timeout=0 name=src src. ! application/x-rtp,media=video,encoding-name=H265 ! rtph265depay ! h265parse config-interval=-1 ! mp4mux faststart=true streamable=true ! filesink location=/{location}.mp4 2>&1"
+        )
+        vv.extend(
+            [
+                "-e",
+                "rtspsrc",
+                f"location=rtsp://127.0.0.1:8554/RTSPWriter_{location}_rgb",
+                "protocols=tcp",
+                "latency=200",
+                "tcp-timeout=0",
+                "name=src",
+                "src.",
+                "!",
+                "application/x-rtp,media=video,encoding-name=H265",
+                "!",
+                "rtph265depay",
+                "!",
+                "h265parse",
+                "config-interval=-1",
+                "!",
+                "mp4mux",
+                "faststart=true",
+                "streamable=true",
+                "!",
+                "filesink",
+                f"location=/{location}.mp4",
+            ]
+        )
+        break
 
-    print(" ".join(live))
+    print("ARGS: ", vv)
+
+    os.execvp(vv[0], vv)
 
 
 if __name__ == "__main__":
