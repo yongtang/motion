25ec68d978485ce73c1adf6e7dbc3393a5231da8
Yong Tang <yong.tang.github@outlook.com>
2025-10-10 03:06:04 +0000

    Fix

--- server/isaac/motion/extension.py
+++ server/isaac/motion/extension.py
@@ -69,10 +69,12 @@ async def run_call(session, call):
     )
     print(f"[motion.extension] [run_call] Articulation: {articulation}")
     import omni.isaac.core.utils.prims as prim_utils
+    from pxr import Usd, PhysxSchema
+
     articulated_prims = []
     for prim in stage.Traverse():
       # Check if prim can be articulated
-      if prim_utils.is_prim_path_articulated(prim.GetPath().pathString):
+      if prim.HasAPI(PhysxSchema.PhysxArticulationRootAPI):
         articulated_prims.append(prim)
 
     print(f"[motion.extension] [run_call] Articulated prims: {[prim.GetPath() for prim in articulated_prims]}")
